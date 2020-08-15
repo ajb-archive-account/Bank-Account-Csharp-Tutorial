056fe7743826a79b7347e0c985f82718c4abc99c
andybyers21 <andy.byers21@gmail.com>
2020-08-15 20:38:17 +0100

    int new branch for testing

--- Program.cs
+++ Program.cs
@@ -10,10 +10,22 @@ namespace BankAccount_Tutorial
             var account = new BankAccount("Dave Davison", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial ballance.");
 
+            // Try some deposits and withdrawls
             account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
             Console.WriteLine(account.Balance);
             account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
             Console.WriteLine(account.Balance);
+
+            // // Make new account for "Poor Dude"
+            // try
+            // {
+            //     var invalidAccount = new BankAccount("invalid", -55);
+            // }
+            // catch (ArgumentOutOfRangeException e)
+            // {
+            //     Console.WriteLine("Exception caught creating account with negative balance");
+            //     Console.WriteLine(e.ToString());
+            // }
         }
     }
 }
