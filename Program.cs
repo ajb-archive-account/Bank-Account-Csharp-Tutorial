db0a6f2224dc3cf5761d2ac12c418c960a81b091
andybyers21 <andy.byers21@gmail.com>
2020-08-15 14:39:24 +0100

    initial commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace BankAccount_Tutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("Dave Davison", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial ballance.");
         }
     }
 }
